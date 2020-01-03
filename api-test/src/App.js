1dc98d17c59268e73e33b9c0e4a832c9ff97c446
adeoye adela <adeoye.adela@gmail.com>
2020-01-03 01:05:01 +0100

    attempting stretch challenge

--- api-test/src/App.js
+++ api-test/src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import SignIn from "./component/SignUp.js";
+import Login from "./component/Login.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" render={props => <SignIn {...props} />} />
+        <Route  path="/login" render={props => <Login {...props} />} />
+       
+      </div>
+    </Router>
   );
 }
 
