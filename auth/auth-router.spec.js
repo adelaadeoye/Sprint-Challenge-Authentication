0afc999ae9c25da042c5fca8e61c275540be2169
adeoye adela <adeoye.adela@gmail.com>
2020-01-04 19:08:42 +0100

    move beforeEach to global

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -9,17 +9,16 @@ const server = require("../api/server");
 const db = require("../database/dbConfig.js");
 
 //Here i want to test the auth endpoint
+beforeEach(() => db("users").truncate());
 describe("/api/auth", () => {
   //Test the register endpoint
-
+  const username = "adela";
+  const password = "adela";
   describe("Post/register", () => {
-    beforeEach(() => db("users").truncate());
 
-    const username = "adela";
-    let password = "adela";
+    
 
-    const hash = bcrypt.hashSync(password, 10); // 2 ^ n
-    password = hash;
+   
 
     it("should return a status code 201 ", () => {
       return request(server)
@@ -40,17 +39,15 @@ describe("/api/auth", () => {
   //Test for login endpoint 
 
   describe('POST/ Login', ()=>{
-      beforeEach(()=>db('users').truncate())
 
       const username = 'testing'
         const password = 'password'
 
-        // bcrypt.compare = jest.fn((a,b,cb) => {cb(false, a === b)})
 
-        it('should return status 200 Ok', async () => {
+        it('should return status 401 ', async () => {
             await db('users').insert({username, password})
             return request(server).post('/api/auth/login').send({username, password})
-                .expect(200)
+                .expect(401)
         })
 
         it('should return json', async () => {
